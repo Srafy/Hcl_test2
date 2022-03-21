b5f1e4a2fe2e065dda33e590f8a78d7d260f5269
Srafy <Srafy_6078@163.com>
2022-03-21 10:22:57 +0800

    test

--- src/TestRun.java
+++ src/TestRun.java
@@ -2,5 +2,8 @@ public class TestRun {
     public static void main(String[] args) {
         System.out.println("TEST");
         System.out.println("提交： Ctrl + K  Commit and Push");
+        System.out.println("更新： Ctrl + T  OK");
+        System.out.println("123");
+        System.out.println("");
     }
 }
